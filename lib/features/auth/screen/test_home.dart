0ceab0aece3b439fd22e6516729b9d1d5ee9114e
Athulya_Ajay <karthaathulya@gmail.com>
2025-06-16 10:24:01 +0530

    App constants change

---
+++ lib/features/auth/screen/test_home.dart
@@ -0,0 +1,25 @@
+import 'package:etutor/common/constants/app_constants.dart';
+import 'package:etutor/features/subscribed_course.dart/widgets/course_card.dart';
+import 'package:flutter/material.dart';
+
+class TestHome extends StatefulWidget {
+  const TestHome({super.key});
+
+  @override
+  State<TestHome> createState() => _TestHomeState();
+}
+
+class _TestHomeState extends State<TestHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Column(
+        children: [
+          
+          // CourseCard(title: "dtfyg", subtitle: "ctfyvgu", icon: Icons.add_ic_call_outlined, Iconbgcolor: Colors.black26),
+          
+        ],
+      ) ,
+    );
+  }
+}
