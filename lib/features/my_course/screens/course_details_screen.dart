32bd20da583d4e1cd1bfac75c5fcfc5288243379
Athulya_Ajay <karthaathulya@gmail.com>
2025-06-30 17:39:39 +0530

    Appbar and list changes

--- lib/features/my_course/screens/course_details_screen.dart
+++ lib/features/my_course/screens/course_details_screen.dart
@@ -33,6 +33,14 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Course Details"),
+        backgroundColor: AppColor.whiteColor,
+        leading: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: CustomBackButton(),
+        ),
+      ),
       backgroundColor: AppColor.whiteColor,
       body: SafeArea(
           child: Stack(children: [
@@ -42,7 +50,6 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen>
               padding: const EdgeInsets.all(16.0),
               child: Stack(
                 children:[ 
-                  CustomBackButton(),
                   Container(
                   height: MediaQuery.of(context).size.width * .5,
                   width: double.infinity,
