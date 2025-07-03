5a5a783b03e4d93002863ec604023b3d93dd74bd
anaswara-venu <anaswara.venu20@gmail.com>
2025-07-03 17:20:50 +0530

    state implimetation in payment screen 50 % complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:etutor/common/screens/splash_screen.dart';
 import 'package:etutor/common/constants/app_constants.dart';
-import 'package:etutor/features/profile/screens/edit_profile.dart';
+import 'package:etutor/features/payment/controller/payment_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tex/flutter_tex.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await TeXRenderingServer.start();
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create:  (_) => PaymentProvider(),
+      child: MyApp(),)
+  );
 }
 
 class MyApp extends StatelessWidget {
