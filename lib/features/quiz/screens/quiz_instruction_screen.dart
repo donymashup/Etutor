6f6781b6e17a88d164210d4d786735e6fece8af1
Midhun Murali <midhunmurali776@gmail.com>
2025-07-03 12:30:55 +0530

    Updates for Quiz Section Screens

--- lib/features/quiz/screens/quiz_instruction_screen.dart
+++ lib/features/quiz/screens/quiz_instruction_screen.dart
@@ -1,5 +1,6 @@
 import 'package:etutor/common/constants/app_constants.dart';
 import 'package:etutor/common/widgets/back_button.dart';
+import 'package:etutor/features/quiz/screen/new_test_page.dart';
 import 'package:etutor/features/quiz/widgets/bullet_point.dart';
 import 'package:flutter/material.dart';
 
@@ -9,88 +10,127 @@ class QuizInstructionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF1F3F6),
-      appBar: AppBar(
-        backgroundColor: AppColor.whiteColor,
-        title: const Text(
-          "Quiz Instructions",
-          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
-        ),
-        leading: const Padding(
-          padding: EdgeInsets.all(10.0),
-          child: CustomBackButton(),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 132,
-              alignment: Alignment.center,
-              color: AppColor.primaryColor,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-              child: const Text(
-                'IQQM EXAM [08-06-2025]',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: AppColor.whiteColor,
-                  fontWeight: FontWeight.w600,
+      backgroundColor: AppColor.whiteColor,
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            alignment: Alignment.center,
+            color: AppColor.primaryColor,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            child: Column(
+              children: [
+                SizedBox(height: 40),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                      child: CustomBackButton(),
+                    ),
+                    Text(
+                      "Quiz Instructions",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 20, color: AppColor.whiteColor),
+                    ),
+                  ],
                 ),
-              ),
+                const Text(
+                  'IQQM EXAM [08-06-2025]',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: AppColor.whiteColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 30),
+              ],
             ),
-            Container(
-              color: AppColor.whiteColor,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Center(
-                  child: SizedBox(
-                    width: 60,
-                    height: 4,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        color: AppColor.primaryColor,
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                      ),
+          ),
+          Container(
+            color: AppColor.whiteColor,
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Center(
+                child: SizedBox(
+                  width: 60,
+                  height: 4,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: AppColor.primaryColor,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
                     ),
                   ),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Brief explanation about this quiz',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      const CircleAvatar(
-                        radius: 22,
-                        backgroundColor: Colors.black87,
-                        child: Icon(Icons.help_outline, color: Colors.white),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Brief explanation about this quiz',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
                       ),
-                      const SizedBox(width: 12),
-                      Expanded(
-                        child: Column(
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 22,
+                          backgroundColor: Colors.black87,
+                          child: Icon(Icons.help_outline, color: Colors.white),
+                        ),
+                        const SizedBox(width: 12),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                '30 Questions',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                '1 point for a correct answer, -1 for a wrong answer',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 22,
+                          backgroundColor: Colors.black87,
+                          child: Icon(Icons.access_time, color: Colors.white),
+                        ),
+                        const SizedBox(width: 12),
+                        Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: const [
                             Text(
-                              '30 Questions',
+                              '30 Minutes',
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 15,
                               ),
                             ),
                             Text(
-                              '1 point for a correct answer, -1 for a wrong answer',
+                              'Total duration of the quiz',
                               style: TextStyle(
                                 fontSize: 13,
                                 color: Colors.grey,
@@ -98,97 +138,69 @@ class QuizInstructionPage extends StatelessWidget {
                             ),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    children: [
-                      const CircleAvatar(
-                        radius: 22,
-                        backgroundColor: Colors.black87,
-                        child: Icon(Icons.access_time, color: Colors.white),
-                      ),
-                      const SizedBox(width: 12),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            '30 Minutes',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                          ),
-                          Text(
-                            'Total duration of the quiz',
-                            style: TextStyle(
-                              fontSize: 13,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24),
-                  const Text(
-                    'Please read the text below carefully so you can understand it',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
+                      ],
                     ),
-                  ),
-                  const SizedBox(height: 12),
-                  const BulletPoint(
-                    text:
-                        'Correct and incorrect marks are shown for each and every questions',
-                  ),
-                  const BulletPoint(
-                    text: 'Tap on options to select the correct answer',
-                  ),
-                  const BulletPoint(
-                    text:
-                        'Save each exam answers before submitting (press save button)',
-                  ),
-                  const BulletPoint(
-                    text: 'Once saved cannot be changed',
-                  ),
-                  const BulletPoint(
-                    text:
-                        'Press submit after saving the answers to complete exam (Press submit and end button)',
-                  ),
-                ],
+                    const SizedBox(height: 24),
+                    const Text(
+                      'Please read the text below carefully so you can understand it',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    const BulletPoint(
+                      text:
+                          'Correct and incorrect marks are shown for each and every questions',
+                    ),
+                    const BulletPoint(
+                      text: 'Tap on options to select the correct answer',
+                    ),
+                    const BulletPoint(
+                      text:
+                          'Save each exam answers before submitting (press save button)',
+                    ),
+                    const BulletPoint(
+                      text: 'Once saved cannot be changed',
+                    ),
+                    const BulletPoint(
+                      text:
+                          'Press submit after saving the answers to complete exam (Press submit and end button)',
+                    ),
+                  ],
+                ),
               ),
             ),
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF0E6ECF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Start Quiz',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            child: SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF0E6ECF),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () => Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                        builder: (context) => TexViewQuizScreen())),
+                child: const Text(
+                  'Start Quiz',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 30)
+        ],
       ),
     );
   }
---
+++ lib/features/quiz/widgets/submission_row.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SubmissionRow extends StatelessWidget {
+  String label;
+  String count;
+  IconData icon;
+  SubmissionRow(this.label, this.count, this.icon, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(icon),
+              SizedBox(width: 10),
+              Text(label),
+            ],
+          ),
+          Text(count, style: TextStyle(fontWeight: FontWeight.w600),),
+        ],
+      ),
+    );
+  }
+}
