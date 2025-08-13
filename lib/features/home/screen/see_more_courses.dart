07dd42bef1e48773c4e528ae745106e2f6446d49
anaswara-venu <anaswara.venu20@gmail.com>
2025-08-13 17:19:00 +0530

    see more screnn with drop down completed

--- lib/features/home/screen/see_more_courses.dart
+++ lib/features/home/screen/see_more_courses.dart
@@ -1,7 +1,10 @@
 import 'package:etutor/common/constants/app_constants.dart';
 import 'package:etutor/common/widgets/back_button.dart';
+import 'package:etutor/features/auth/provider/login_provider.dart';
+import 'package:etutor/features/home/provider/homepage_provider.dart';
 import 'package:etutor/features/my_course/widgets/mycoursecard.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SeeMoreCourses extends StatefulWidget {
   const SeeMoreCourses({super.key});
@@ -13,129 +16,132 @@ class SeeMoreCourses extends StatefulWidget {
 class _SeeMoreCoursesState extends State<SeeMoreCourses> {
   String? classDropdownValue;
   List<Map<String, dynamic>> filteredclass = [];
-  final List<String> _class = [
-    "class 5",
-    "class 6",
-    "class 7",
-    "class 8",
-    "class 9",
-    "class 10",
-    "class 11",
-    "class 12",
-    "All Courses"
-  ];
-  final List<Map<String, dynamic>> courses = const [
-    {
-      'title': ' International General Knowledge Olympiad (IGKO)',
-      'image': 'assets/images/oly2.jpg',
-      'isFree': false,
-      'rating': 4.6,
-      'class': 'class 6',
-    },
-    {
-      'title': 'International Commerce Olympiad (ICO)',
-      'image': 'assets/images/oly3.jpeg',
-      'isFree': false,
-      'rating': 4.4,
-      'class': 'class 6',
-    },
-    {
-      'title': 'International English Olympiad (IEO)',
-      'image': 'assets/images/oly4.jpg',
-      'isFree': false,
-      'rating': 4.7,
-      'class': 'class 7',
-    },
-    {
-      'title': 'Mental Ability 25-26',
-      'image': 'assets/images/oly8.jpg',
-      'isFree': true,
-      'rating': 4.8,
-      'class': 'class 6',
-    },
-    {
-      'title': ' International Hindi Olympiad (IEO)',
-      'image': 'assets/images/oly11.jpg',
-      'isFree': false,
-      'rating': 4.3,
-      'class': 'class 10',
-    },
-    {
-      'title': 'International Social Studies Olympiad (ISSO)',
-      'image': 'assets/images/oly1.jpg',
-      'isFree': false,
-      'rating': 4.5,
-      'class': 'class 8',
-    },
-    {
-      'title': 'Math Olympiad 25-26',
-      'image': 'assets/images/oly9.jpg',
-      'rating': 4.6,
-      'isFree': true,
-      'class': 'class 9',
-    },
-    {
-      'title': 'Spell Bee (CSB) ',
-      'image': 'assets/images/oly10.jpg',
-      'rating': 4.9,
-      'isFree': false,
-      'class': 'class 7',
-    },
-    {
-      'title': 'National Interactive Maths Olympiad [NIMO]',
-      'image': 'assets/images/oly6.jpeg',
-      'isFree': true,
-      'rating': 4.9,
-      'class': 'class 8',
-    },
-    {
-      'title': 'International Mathematics Olympiad (IMO)',
-      'image': 'assets/images/oly5.webp',
-      'isFree': false,
-      'rating': 4.8,
-      'class': 'class 9',
-    },
-    {
-      'image': 'assets/images/oly7.png',
-      'title': 'National Science Olympiad (NSO)',
-      'isFree': true,
-      'rating': 4.6,
-      'class': 'class 10',
-    },
-    {
-      'title': 'English Smart Series',
-      'image': 'assets/images/oly6.jpeg',
-      'isFree': true,
-      'rating': 4.3,
-      'class': 'class 7',
-    },
-  ];
+  HomepageProvider homeProvider = HomepageProvider();
+  // final List<String> _class = [
+  //   "class 5",
+  //   "class 6",
+  //   "class 7",
+  //   "class 8",
+  //   "class 9",
+  //   "class 10",
+  //   "class 11",
+  //   "class 12",
+  //   "All Courses"
+  // ];
+  // final List<Map<String, dynamic>> courses = const [
+  //   {
+  //     'title': ' International General Knowledge Olympiad (IGKO)',
+  //     'image': 'assets/images/oly2.jpg',
+  //     'isFree': false,
+  //     'rating': 4.6,
+  //     'class': 'class 6',
+  //   },
+  //   {
+  //     'title': 'International Commerce Olympiad (ICO)',
+  //     'image': 'assets/images/oly3.jpeg',
+  //     'isFree': false,
+  //     'rating': 4.4,
+  //     'class': 'class 6',
+  //   },
+  //   {
+  //     'title': 'International English Olympiad (IEO)',
+  //     'image': 'assets/images/oly4.jpg',
+  //     'isFree': false,
+  //     'rating': 4.7,
+  //     'class': 'class 7',
+  //   },
+  //   {
+  //     'title': 'Mental Ability 25-26',
+  //     'image': 'assets/images/oly8.jpg',
+  //     'isFree': true,
+  //     'rating': 4.8,
+  //     'class': 'class 6',
+  //   },
+  //   {
+  //     'title': ' International Hindi Olympiad (IEO)',
+  //     'image': 'assets/images/oly11.jpg',
+  //     'isFree': false,
+  //     'rating': 4.3,
+  //     'class': 'class 10',
+  //   },
+  //   {
+  //     'title': 'International Social Studies Olympiad (ISSO)',
+  //     'image': 'assets/images/oly1.jpg',
+  //     'isFree': false,
+  //     'rating': 4.5,
+  //     'class': 'class 8',
+  //   },
+  //   {
+  //     'title': 'Math Olympiad 25-26',
+  //     'image': 'assets/images/oly9.jpg',
+  //     'rating': 4.6,
+  //     'isFree': true,
+  //     'class': 'class 9',
+  //   },
+  //   {
+  //     'title': 'Spell Bee (CSB) ',
+  //     'image': 'assets/images/oly10.jpg',
+  //     'rating': 4.9,
+  //     'isFree': false,
+  //     'class': 'class 7',
+  //   },
+  //   {
+  //     'title': 'National Interactive Maths Olympiad [NIMO]',
+  //     'image': 'assets/images/oly6.jpeg',
+  //     'isFree': true,
+  //     'rating': 4.9,
+  //     'class': 'class 8',
+  //   },
+  //   {
+  //     'title': 'International Mathematics Olympiad (IMO)',
+  //     'image': 'assets/images/oly5.webp',
+  //     'isFree': false,
+  //     'rating': 4.8,
+  //     'class': 'class 9',
+  //   },
+  //   {
+  //     'image': 'assets/images/oly7.png',
+  //     'title': 'National Science Olympiad (NSO)',
+  //     'isFree': true,
+  //     'rating': 4.6,
+  //     'class': 'class 10',
+  //   },
+  //   {
+  //     'title': 'English Smart Series',
+  //     'image': 'assets/images/oly6.jpeg',
+  //     'isFree': true,
+  //     'rating': 4.3,
+  //     'class': 'class 7',
+  //   },
+  // ];
 
   @override
   void initState() {
     super.initState();
-    filteredclass = courses;
+   // filteredclass = courses;
   }
 
-void _filterByclass(String query) {
-    if (query.isEmpty || query.toLowerCase() == "all courses") {
-      setState(() {
-        filteredclass =courses; 
-      });
-    }else {
-      setState(() {
-        filteredclass = courses.where((item) {
-          return item['class']!
-              .toLowerCase()
-              .contains(query.toLowerCase());
-        }).toList();
-      });
-    }
-  }
+  //  void _filterByclass(String query) {
+  //   if (query.isEmpty || query.toLowerCase() == "all courses") {
+  //     setState(() {
+  //       filteredclass =courses; 
+  //     });
+  //   }else {
+  //     setState(() {
+  //       filteredclass = courses.where((item) {
+  //         return item['class']!
+  //             .toLowerCase()
+  //             .contains(query.toLowerCase());
+  //       }).toList();
+  //     });
+  //   }
+  // }
   
 
   @override
   Widget build(BuildContext context) {
+    homeProvider = Provider.of<HomepageProvider>(context, listen: true);
+    List<String> classList = context.read<LoginProvider>().classes.map((c) => c.name).whereType<String>().toList();
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -193,7 +199,7 @@ void _filterByclass(String query) {
                       color: AppColor.greyText, fontWeight: FontWeight.w400),
                 ),
                 value: classDropdownValue,
-                items: _class.map<DropdownMenuItem<String>>((String value) {
+                items: classList.map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
                     child: Text(value,
@@ -202,7 +208,7 @@ void _filterByclass(String query) {
                   );
                 }).toList(),
                   onChanged: (String? newValue) {
-                   _filterByclass(newValue!);
+                 //  _filterByclass(newValue!);
                   setState(() {
                     classDropdownValue = newValue;
                    
@@ -210,17 +216,17 @@ void _filterByclass(String query) {
                 },
               ),
             ),
-            filteredclass.isEmpty ? Center(child: Text("No Matching results found")):
+            homeProvider.syllabusCourse.isEmpty ? Center(child: Text("No Matching results found")):
             Wrap(
               spacing: 10,
               alignment: WrapAlignment.start,
               children: [
                 ...List.generate(
-                    filteredclass.length,
+                    homeProvider.syllabusCourse.length,
                     (index) => MyCourseCard(
-                          title: filteredclass[index]['title'],
-                          imagePath: filteredclass[index]['image'],
-                          rating: filteredclass[index]['rating'],
+                          title: homeProvider.syllabusCourse[index].name ?? "",
+                          imagePath:homeProvider.syllabusCourse[index].image ?? "",
+                          rating:  double.tryParse(homeProvider.syllabusCourse[index].avgStars.toString()) ?? 0.0,
                           //isFree: filteredclass[index]['isFree'],
                         ))
               ],
